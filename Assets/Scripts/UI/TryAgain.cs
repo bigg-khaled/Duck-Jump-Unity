4daa7419441f4901ca3fbd120a4f80d5d04a748b
EyadTamer999 <eyadhassan2002@gmail.com>
2023-07-23 23:41:00 +0300

    added Game over UI, added restart level with functionality, and added revive

---
+++ Assets/Scripts/UI/TryAgain.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TryAgain : MonoBehaviour
+{
+    //onclick, reload the scene
+    public void TryAgainLevel()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+}
