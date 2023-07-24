b651bc44a90af998d2266de2ab14a4f303fa75f3
EyadTamer999 <eyadhassan2002@gmail.com>
2023-07-24 22:52:39 +0300

    added shit to menu and added share button

--- Assets/Scripts/UI/ShareButton.cs
+++ Assets/Scripts/UI/ShareButton.cs
@@ -1,81 +1,51 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System;
 using System.Collections;
-
+using System.Collections.Generic;
+using System.IO;
 
 public class ShareButton : MonoBehaviour
 {
-    public Button shareButton;
-    private bool isFocus = false;
-    private bool isProcessing = false;
+    [SerializeField] GameObject Panel_share; //panel to share the score
+    [SerializeField] Text txtPanelScore; //score
+    [SerializeField] Text txtHomeScore; //score in home screen
+    [SerializeField] Text txtDate; //date of the score
 
-    void Start()
+
+
+    public void ShareScore ()
     {
-        shareButton.onClick.AddListener(ShareText);
+        txtPanelScore.text = txtHomeScore.text; //get the same score in home sceen
+        DateTime dt = DateTime.Now; //get the current date
+
+        txtDate.text = string.Format ("{0}/{1}/{2}", dt.Month, dt.Day, dt.Year);
+
+        //open the score panel
+        Panel_share.SetActive (true);//show the panel
+        StartCoroutine ("TakeScreenShotAndShare");
     }
 
-    void OnApplicationFocus(bool focus)
+    IEnumerator TakeScreenShotAndShare ()
     {
-        isFocus = focus;
+        yield return new WaitForEndOfFrame ();
+
+        Texture2D tx = new Texture2D (Screen.width, Screen.height, TextureFormat.RGB24, false);
+        tx.ReadPixels (new Rect (0, 0, Screen.width, Screen.height), 0, 0);
+        tx.Apply ();
+
+        string path = Path.Combine (Application.temporaryCachePath, "sharedImage.png");//image name
+        File.WriteAllBytes (path, tx.EncodeToPNG ());
+
+        Destroy (tx); //to avoid memory leaks
+
+        new NativeShare ()
+            .AddFile (path)
+            .SetSubject ("This is my score")
+            .SetText ("share your score with your friends")
+            .Share ();
+
+
+        Panel_share.SetActive (false); //hide the panel
     }
-
-    private void ShareText()
-    {
-
-#if UNITY_ANDROID
-
-        if (!isProcessing)
-        {
-            StartCoroutine(ShareTextInAnroid());
-        }
-
-#else
-		Debug.Log("No sharing set up for this platform.");
-#endif
-
-    }
-
-#if UNITY_ANDROID
-
-    public IEnumerator ShareTextInAnroid()
-    {
-        var shareSubject = "Play POP IT on your phone"; //Subject text
-        var shareMessage = "Get POP IT game from this link: " + //Message text
-                           "https://play.google.com/store/apps/"; //Your link
-
-        isProcessing = true;
-
-        if (!Application.isEditor)
-        {
-            //Create intent for action send
-            AndroidJavaClass intentClass =
-                new AndroidJavaClass("android.content.Intent");
-            AndroidJavaObject intentObject =
-                new AndroidJavaObject("android.content.Intent");
-            intentObject.Call<AndroidJavaObject>
-                ("setAction", intentClass.GetStatic<string>("ACTION_SEND"));
-            //put text and subject extra
-            intentObject.Call<AndroidJavaObject>("setType", "text/plain");
-
-            intentObject.Call<AndroidJavaObject>
-                ("putExtra", intentClass.GetStatic<string>("EXTRA_SUBJECT"), shareSubject);
-            intentObject.Call<AndroidJavaObject>
-                ("putExtra", intentClass.GetStatic<string>("EXTRA_TEXT"), shareMessage);
-
-            //call createChooser method of activity class
-            AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-
-            AndroidJavaObject currentActivity =
-                unity.GetStatic<AndroidJavaObject>("currentActivity");
-            AndroidJavaObject chooser =
-                intentClass.CallStatic<AndroidJavaObject>
-                ("createChooser", intentObject, "Share your high score");
-            currentActivity.Call("startActivity", chooser);
-        }
-
-        yield return new WaitUntil(() => isFocus);
-        isProcessing = false;
-    }
-
-#endif
 }
